50404c74ca9995cbf08b06893bc8b0248f8de9b1
Sarah Whitfield <s.whitfield@meridian.internal>
2026-08-26 11:10:00 -0400

    MOL-4474 faq: replace fxLayout column with flex and gap

--- retail-web/src/app/features/help/components/faq/faq.component.ts
+++ retail-web/src/app/features/help/components/faq/faq.component.ts
@@ -4,6 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'mol-faq',
   templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FaqComponent {}
